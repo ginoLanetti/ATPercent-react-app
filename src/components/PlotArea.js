8d76280015a944a1ce0feefa4a8828438d1fad56
ginoLanetti <alfonsichqu@gmail.com>
2019-09-24 21:37:18 +0200

    changed victory to react-vis'
    pwd

--- src/components/PlotArea.js
+++ src/components/PlotArea.js
@@ -1,39 +1,59 @@
 import React from 'react'
 import PlotStyle from '../styles/PlotStyle'
-import { VictoryTheme, VictoryChart, VictoryLine, VictoryVoronoiContainer} from 'victory';
-
+import {curveCatmullRom} from 'd3-shape'
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, LineMarkSeries, Hint} from 'react-vis'
 
 class PlotArea extends React.Component {
     constructor(){
         super();
         this.state = {
+            value: null,
             data: [
-                { x: 1, y: 1 },
-                { x: 2, y: 2 },
-                { x: 3, y: 4 },
-                { x: 4, y: 7 },
-                { x: 5, y: 11 }
+                {x: 0, y: 8},
+                {x: 1, y: 5},
+                {x: 2, y: 4},
+                {x: 4, y: 1},
+                {x: 4.02, y: 1},
+                {x: 6, y: 6},
+                {x: 7, y: 3},
+                {x: 8, y: 2},
+                {x: 9, y: 0},
               ]
         }
     }
+    forgetValue = () => {
+      this.setState({
+        value: null
+      });
+    };
+  
+    rememberValue = value => {
+      this.setState({value});
+    };
     render(){
+        const {value} = this.state;
         return(
             <PlotStyle>
-                <VictoryChart
-                    theme={VictoryTheme.material}
-                    width={600}
-                    height={500}
-                    containerComponent={
-                        <VictoryVoronoiContainer labels={({ datum }) => `${datum.x}# bp, AT content:${datum.y}%`}/>
-                    }
-                >
-                <VictoryLine 
-                data={this.state.data} 
-                style={{data: { stroke: "#c43a31" }}}
-                animate={{duration: 100}} 
-                interpolation="monotoneX" 
+              <XYPlot width={1000} height={500} className="plotArea">
+                <HorizontalGridLines style={{stroke: '#B7E9ED'}} />
+                <VerticalGridLines style={{stroke: '#B7E9ED'}} />
+                <XAxis
+                  title="position"
+                  style={{
+                    line: {stroke: '#ADDDE1'},
+                    ticks: {stroke: '#ADDDE1'},
+                    text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
+                  }}
                 />
-                </VictoryChart>
+                <YAxis title="AT content" />
+                <LineMarkSeries
+                  curve={curveCatmullRom.alpha(0.5)}
+                  data={this.state.data}
+                  onNearestXY={this.rememberValue}
+                  size={1}
+                />
+                {value ? <Hint value={value} /> : null}
+              </XYPlot>
             </PlotStyle>
         )
     }
--- src/styles/PlotStyle.js
+++ src/styles/PlotStyle.js
@@ -2,5 +2,8 @@ import styled from 'styled-components'
 
 const InputStyle = styled.div`
   width: 60vw;
+  .plotArea{
+    margin: 0 auto;
+  }
 `
 export default InputStyle
