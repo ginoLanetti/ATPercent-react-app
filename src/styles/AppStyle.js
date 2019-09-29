ffeabc2c58326adaedd0699442d6749158ab86e1
ginoLanetti <alfonsichqu@gmail.com>
2019-09-29 20:09:21 +0200

    messed around with chart interactivity

--- src/styles/AppStyle.js
+++ src/styles/AppStyle.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 
 const AppStyle = styled.div`
-  margin: 10vh auto;
+  margin: 10vh auto ;
   width: 60vw;
+  height: 80vh;
   background: #FFF;
   -webkit-box-shadow: -4px 4px 15px -4px rgba(36,36,36,1);
   -moz-box-shadow: -4px 4px 15px -4px rgba(36,36,36,1);
--- src/styles/PlotStyle.js
+++ src/styles/PlotStyle.js
@@ -2,8 +2,18 @@ import styled from 'styled-components'
 
 const InputStyle = styled.div`
   width: 60vw;
+  height: 33vh;
+  display: flex;
+  justify-content: space-between;
   .plotArea{
     margin: 0 auto;
+  }
+  .chart{
+    width:100%;
+    height:100%;
+    
+  }
+  .legend{
   }
 `
 export default InputStyle
